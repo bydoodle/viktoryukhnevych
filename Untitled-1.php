06ea434e4ec64d80f22227a0eb4685a378022452
Viktor Yukhnevych <vyukhnevych@edu.cdv.pl>
2023-03-07 18:02:25 +0100

    main info updated

--- Untitled-1.php
+++ Untitled-1.php
@@ -1,4 +1,27 @@
 <?php
-    $firstName = 'Janusz';
-    $lastName = 'Janusz';
-    echo "Imie i nazwisko: $firstName $lastName";
+$firstName = 'Janusz';
+$lastName = 'Nowak';
+echo "Imie i nazwisko: $firstName $lastName<br>";
+echo 'Imie i nazwisko: $firstName $lastName<br>';
+
+//heredoc
+echo <<<DATA
+    Imie: $firstName<br>
+    Nazwisko: $lastName
+    <hr>
+DATA;
+
+$data = <<<DATA
+    Imie: $firstName<br>
+    Nazwisko: $lastName
+    <hr>
+DATA;
+
+echo $data;
+
+//nowdoc
+echo <<<DATA
+    Imie: $firstName<br>
+    Nazwisko: $lastName
+    <hr>
+DATA;
